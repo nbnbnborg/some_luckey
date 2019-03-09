82c2f75a6c7cc308dbde86b176649e8791390732
nbnbnborg <nbnbnb.org@yandex.com>
2019-03-09 13:05:57 +0800

    UTF-8

--- sh/clear_log/1.sh
+++ sh/clear_log/1.sh
@@ -5,20 +5,20 @@ ERROR_ROOT=86
 ROOT_UID=0
 LINES=50
 ERROR_LOG=87
-#�ж�ִ�нű����û��ǲ���root����Ա�������򷵻ش������
+#判断执行脚本的用户是不是root管理员，不是则返回错误代码
 if [ "$UID" -ne "${ROOT_UID}" ];then
         echo "This script must be run as root"
         exit ${ERROR_ROOT}
 fi
 
-#�ж��������Ƿ����������û�����������Ĭ�ϱ�����־�����50��
+#判断命令行是否输入参数，没有输入参数，默认保存日志的最后50行
 if [ -n "$1" ];then
         lines=$1
 else
         lines=${LINES}
 fi
 
-#���뵽��־Ŀ¼��������ص���չ���
+#进入到日志目录，进行相关的清空工作
 cd ${LOG_DIR}
 if [ `pwd` != "${LOG_DIR}" ];then
         echo "Cannot change to the ${LOG_DIR}"
